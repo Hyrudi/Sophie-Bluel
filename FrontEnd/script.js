9b1ef840335c7760d34b511ffed9c4c8cd9b946f
shirley dereuse <shirleyders@outlook.fr>
2025-07-21 21:32:27 +0200

    wip

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -29,12 +29,43 @@
    getWorks();
    //reponse à notre fonction
 
-
+  
    // filtres 
 
-  
+  function getFilters(){
 
-  
+        fetch("http://localhost:5678/api/categories/").then(response=>{
+        console.log(response)
+         return response.json();
+        }).then(data=>{
+            console.log(data)
+            for (const objets of data) {
+                generateFilters(objets);
+            }
+                
+        })
+   } 
+
+        function generateFilters(name){
+        const input = document.createElement("input")
+
+        
+        input.name = value; //  
+        filters = document.getElementById("filters");
+        filters.appendChild(input);
+        
+    }
+     
+
+    getFilters();
+
+    //Creer btn "Tous"
+
+    const btnTous = document.createElement ("input");
+    btnTous.textContent = "Tous";
+    btnTous.value = 0;
+    btnTous.appendChild(input);
+    
 
 
 
